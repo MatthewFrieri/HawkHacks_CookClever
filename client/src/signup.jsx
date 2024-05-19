dfa53fe6649c0e0c623d497590ed3270cbdfea5e
Matthew Frieri <mat.frieri@gmail.com>
2024-05-19 09:03:50 -0400

    signup page ui done

--- client/src/signup.jsx
+++ client/src/signup.jsx
@@ -44,35 +44,33 @@ function Signup() {
   };
 
   return (
-    <MDBContainer fluid className="signup-container">
-      <MDBRow className='d-flex justify-content-center align-items-center h-100'>
-        <MDBCol col='12'>
-          <MDBCard className='bg-dark text-white my-5 mx-auto signup-card'>
-            <MDBCardBody className='p-5 d-flex flex-column align-items-center mx-auto w-100'>
-              <h2 className="fw-bold mb-2 text-uppercase">Sign Up</h2>
-              <p className="text-white-50 mb-5">Please enter your details to sign up!</p>
-              <div className="input-wrapper mb-4 w-100">
-                <MDBInput value={usernameValue} onChange={(e) => {setUsernameValue(e.target.value)}} labelClass='text-white' label='Username' id='formControlLgEmail' type='email' size="lg" />
-              </div>
-              <div className="input-wrapper mb-4 w-100">
-                <MDBInput value={passwordValue} onChange={(e) => {setPasswordValue(e.target.value)}} labelClass='text-white' label='Password' id='formControlLgPassword' type='password' size="lg" />
-              </div>
-              <div className="input-wrapper mb-4 w-100">
-                <MDBInput value={confirmPasswordValue} onChange={(e) => {setConfirmPasswordValue(e.target.value)}} labelClass='text-white' label='Confirm Password' id='formControlLgConfirmPassword' type='password' size="lg" />
-              </div>
-              <MDBBtn outline className='mx-2 px-5 signup-button' color='white' size='lg' onClick={handleSignup}>
-                Sign Up
-              </MDBBtn>
-              <div>
-                <p className="mb-0">
-                  Already have an account? <a href="/" className="text-white-50 fw-bold">Login</a>
-                </p>
-              </div>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
+    <div id="big-container">
+      <header>
+        <h2>Sign Up</h2>
+      </header>
+      <div id="square-box">
+        <p>Please enter your details to sign up!</p>
+        <span>
+          <p>Username</p>
+          <input value={usernameValue} onChange={(e) => {setUsernameValue(e.target.value)}} />
+        </span>
+        <span>
+          <p>Password</p>
+          <MDBInput value={passwordValue} onChange={(e) => {setPasswordValue(e.target.value)}}t ype='password' />
+        </span>
+        <span>
+          <p>Confirm Password</p>
+          <MDBInput value={confirmPasswordValue} onChange={(e) => {setConfirmPasswordValue(e.target.value)}} type='password' />
+        </span>
+        <button onClick={handleSignup}>Sign Up</button>
+      </div>
+      <div>
+        <p>
+          Already have an account? <a href="/" >Login</a>
+        </p>
+      </div>
+    </div>
+  
   );
 }
 
