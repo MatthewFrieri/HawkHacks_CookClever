17200a2f7de48306e03e2b8ccbfd32ab26b45f97
Masood <masood.ashrafi17@gmail.com>
2024-05-18 22:02:11 -0400

    New Home Page Changes

--- client/src/home.jsx
+++ client/src/home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import pastaImage from './assets/pasta.png';
-import roastChickenImage from './assets/roastchicken.png'; 
+import roastChickenImage from './assets/roastchicken.png';
+import burritoBowl from './assets/burritobowl.png';
+import sandwich from './assets/sandwich.png';
+import './home.css'; // Make sure to import the CSS file
 
 const Home = () => {
   return (
+    
     <div className="container">
       <header>
         <button className="homeButton" onClick={() => window.location.href = '/'}>
@@ -11,17 +15,31 @@ const Home = () => {
         </button>
       </header>
       <main>
-        <div className="content">
-          <img src={pastaImage} alt="Picture of pasta" className="image" /> 
-          <p className="description">
-            This is a picture of a delicious pasta. It's a traditional dish with a rich history and amazing taste.
-          </p>
-        </div>
-        <div className="content">
-          <img src={roastChickenImage} alt="Roast Chicken" className="image" />
-          <p className="description">
-            This is a picture of roast chicken. It's a well-loved dish, perfect for any occasion.
-          </p>
+        <div className="grid">
+          <div className="content">
+            <img src={pastaImage} alt="Picture of pasta" className="image" />
+            <p className="description">
+              This is a picture of a delicious pasta. It's a traditional dish with a rich history and amazing taste.
+            </p>
+          </div>
+          <div className="content">
+            <img src={roastChickenImage} alt="Roast Chicken" className="image" />
+            <p className="description">
+              This is a picture of roast chicken. It's a well-loved dish, perfect for any occasion.
+            </p>
+          </div>
+          <div className="content">
+            <img src={burritoBowl} alt="Burrito Bowl" className="image" />
+            <p className="description">
+              This is a picture of a burrito bowl. A healthy and tasty choice for any meal.
+            </p>
+          </div>
+          <div className="content">
+            <img src={sandwich} alt="Sandwich" className="image" />
+            <p className="description">
+              This is a picture of a sandwich. Perfect for a quick and delicious snack.
+            </p>
+          </div>
         </div>
       </main>
     </div>
